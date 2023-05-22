d9e40d2f4b7bb1d3165468f48c14826af0cc6964
19745323 <NALisovik@sberbank.ru>
2023-05-22 13:57:35 +0300

    add components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React, { useState } from "react";
 import Counter from "./components/Counter";
 import "./styles/App.css";
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+
+
+
 function App() {
-  const [value, setValue] = useState("Текст в инпуте");
+  const [posts, setPosts] = useState([
+      {id: 1, title: 'JavaScript', body: 'Description'},
+      {id: 2, title: 'JavaScript 2', body: 'Description'},
+      {id: 3, title: 'JavaScript 3', body: 'Description'}
+  ])
 
   return (
     <div className="App">
-      <div className="post">
-        <div className="post_content">
-          <strong>1. Javascript</strong>
-          <div>Javascript - язык программирования</div>
-        </div>
-        <div className="post_btns">
-          <button>Удалить</button>
-        </div>
-      </div>
+        <form>
+            <input type='text' placeholder='Название поста'/>
+            <input type='text' placeholder='Описание поста'/>
+            <button>Создать пост</button>
+        </form>
+
+        <PostList posts={posts} title="Список постов 1"/>
     </div>
   );
 }
