8da3d84ccec81e3d1bc333b070d3ee9cdbf31c99
Lisstrange <nik.lis@bk.ru>
2023-05-27 22:25:31 +0300

    onChange

--- src/App.js
+++ src/App.js
@@ -1,53 +1,62 @@
-import React, {useRef, useState} from "react";
+import React, { useRef, useState } from "react";
 import Counter from "./components/Counter";
 import "./styles/App.css";
 import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
-import {PROPERTY_TYPES} from "@babel/types";
+import { PROPERTY_TYPES } from "@babel/types";
 import PostForm from "./components/PostForm";
 import MySelect from "./components/UI/select/MySelect";
-
+import { sort } from "semver";
 
 function App() {
-    const [posts, setPosts] = useState([
-        {id: 1, title: "JavaScript", body: "Description"},
-        {id: 2, title: "JavaScript 2", body: "Description"},
-        {id: 3, title: "JavaScript 3", body: "Description"},
-    ]);
+  const [posts, setPosts] = useState([
+    { id: 1, title: "фф", body: "ьь" },
+    { id: 2, title: "ыы 2", body: "вв" },
+    { id: 3, title: "ррр 3", body: "яяы" },
+  ]);
 
+  // Сортировка
+  const [selectedSort, setSelectedSort] = useState("");
 
-    const createPost = (newPost) => {
-        setPosts([...posts, newPost])
-    }
-    // const bodyInputRef = useRef(); Не управляемая хуйня
+  const sortPosts = (sort) => {
+    setSelectedSort(sort);
+    console.log(sort);
+    // setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])));
+  };
 
-    const removePost = (post) => {
-        setPosts(posts.filter(p => p.id !== post.id))
-    }
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost]);
+  };
+  // const bodyInputRef = useRef(); Не управляемая хуйня
 
-    return (
-        <div className="App">
-            <PostForm create={createPost}/>
-            <hr style={{margin: '15px 0'}}/>
+  const removePost = (post) => {
+    setPosts(posts.filter((p) => p.id !== post.id));
+  };
 
-            <div>
-
-                <MySelect
-                    defaultValue="Сортировка"
-                    options={[
-                        {value: 'title', name: 'По названию'},
-                        {value: 'body', name: 'По описанию'},
-                    ]}
-                />
-            </div>
-            {posts.length
-                ? <PostList remove={removePost} posts={posts} title="Список постов 1"/>
-                : <h1 style={{textAlign: 'center'}}>Посты не найдены!</h1>}
-
-        </div>
-    );
+  return (
+    <div className="App">
+      <PostForm create={createPost} />
+      <hr style={{ margin: "15px 0" }} />
+      <div>
+        <MySelect
+          value={selectedSort}
+          onChange={sortPosts}
+          defaultValue="Сортировка"
+          options={[
+            { value: "title", name: "По названию" },
+            { value: "body", name: "По описанию" },
+          ]}
+        />
+      </div>
+      {posts.length ? (
+        <PostList remove={removePost} posts={posts} title="Список постов 1" />
+      ) : (
+        <h1 style={{ textAlign: "center" }}>Посты не найдены!</h1>
+      )}
+    </div>
+  );
 }
 
 export default App;
--- src/components/UI/select/MySelect.jsx
+++ src/components/UI/select/MySelect.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 
-const MySelect = ({options, defaultValue}) => {
-    return (
-        <select>
-            <option disabled value="">{defaultValue}</option>
-            {options.map(option =>
-                <option value={option.value}>
-                    {option.name}
-                </option>
-            )}
-        </select>
-    )
+const MySelect = ({ options, defaultValue, value, onChange }) => {
+  return (
+    <select value={value} onChange={(event) => onChange(event.target.value)}>
+      <option disabled>{defaultValue}</option>
+      {options.map((option) => (
+        <option key={option.value} value={option.value}>
+          {option.name}
+        </option>
+      ))}
+    </select>
+  );
+};
 
-}
-
-export default MySelect
+export default MySelect;
