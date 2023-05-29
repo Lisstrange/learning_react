12c39d588b3a6d9d95c74fe2c83a4c5b5e67d6f8
19745323 <NALisovik@sberbank.ru>
2023-05-29 17:36:47 +0300

    last checkpoint

--- src/App.js
+++ src/App.js
@@ -1,67 +1,19 @@
-import React, { useEffect, useMemo, useRef, useState } from "react";
+import React, {useEffect, useMemo, useRef, useState} from "react";
 import "./styles/App.css";
-import PostList from "./components/PostList";
-import MyButton from "./components/UI/button/MyButton";
-import PostForm from "./components/PostForm";
-import PostFilter from "./components/PostFilter";
-import MyModal from "./components/UI/MyModal/MyModal";
-import { usePosts } from "./hooks/usePost";
-import { async } from "q";
-import axios from "axios";
-import { useFetching } from "./hooks/useFetching";
+import {BrowserRouter, Routes, Route, Link, Navigate} from "react-router-dom";
+import About from "./pages/About";
+import Posts from "./pages/Posts";
+import Navbar from "./components/UI/Navbar/Navbar";
 
 function App() {
-  const [posts, setPosts] = useState([]);
-
-  const [filter, setFilter] = useState({ sort: "", query: "" });
-  const [modal, setModal] = useState(false);
-  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-
-  const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
-    const response = await axios.get(
-      "https://jsonplaceholder.typicode.com/users/1/posts"
-    );
-    setPosts(response.data);
-  });
-
-  useEffect(() => {
-    fetchPosts();
-  }, []);
-
-  const createPost = (newPost) => {
-    setPosts([...posts, newPost]);
-    setModal(false);
-  };
-
-  const removePost = (post) => {
-    setPosts(posts.filter((p) => p.id !== post.id));
-  };
-
-  return (
-    <div className="App">
-      <MyButton style={{ marginTop: 30 }} onClick={() => setModal(true)}>
-        Создать пост
-      </MyButton>
-      <MyModal visible={modal} setVisible={setModal}>
-        <PostForm create={createPost} />
-      </MyModal>
-
-      <hr style={{ margin: "15px 0" }} />
-      <PostFilter filter={filter} setFilter={setFilter} />
-      {postError && <h1> Произошла ошибка </h1>}
-      {isPostsLoading ? (
-        <div style={{ display: "center" }}>
-          <h1>Загрузка...</h1>
-        </div>
-      ) : (
-        <PostList
-          remove={removePost}
-          posts={sortedAndSearchedPosts}
-          title="Посты про JS"
-        />
-      )}
-    </div>
-  );
+    return (<BrowserRouter>
+        <Navbar/>
+        <Routes>
+            <Route path='about' element={<About/>}/>
+            <Route path='posts' element={<Posts/>}/>
+        </Routes>
+        <Navigate to='posts'/>
+    </BrowserRouter>)
 }
 
 export default App;
---
+++ src/pages/Posts.jsx
@@ -0,0 +1,66 @@
+import React, { useEffect, useMemo, useRef, useState } from "react";
+import {useFetching} from "../hooks/useFetching";
+import axios from "axios";
+import MyButton from "../components/UI/button/MyButton";
+import MyModal from "../components/UI/MyModal/MyModal";
+import PostForm from "../components/PostForm";
+import PostFilter from "../components/PostFilter";
+import PostList from "../components/PostList";
+import {usePosts} from "../hooks/usePost";
+
+
+function Posts() {
+  const [posts, setPosts] = useState([]);
+
+  const [filter, setFilter] = useState({ sort: "", query: "" });
+  const [modal, setModal] = useState(false);
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+
+  const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/users/1/posts"
+    );
+    setPosts(response.data);
+  });
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost]);
+    setModal(false);
+  };
+
+  const removePost = (post) => {
+    setPosts(posts.filter((p) => p.id !== post.id));
+  };
+
+  return (
+    <div className="App">
+      <MyButton style={{ marginTop: 30 }} onClick={() => setModal(true)}>
+        Создать пост
+      </MyButton>
+      <MyModal visible={modal} setVisible={setModal}>
+        <PostForm create={createPost} />
+      </MyModal>
+
+      <hr style={{ margin: "15px 0" }} />
+      <PostFilter filter={filter} setFilter={setFilter} />
+      {postError && <h1> Произошла ошибка </h1>}
+      {isPostsLoading ? (
+        <div style={{ display: "center" }}>
+          <h1>Загрузка...</h1>
+        </div>
+      ) : (
+        <PostList
+          remove={removePost}
+          posts={sortedAndSearchedPosts}
+          title="Посты про JS"
+        />
+      )}
+    </div>
+  );
+}
+
+export default Posts;
